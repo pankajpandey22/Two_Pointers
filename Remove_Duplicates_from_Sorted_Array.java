c121bfd173c135e0289d388f896c6e3e2526c61f
pankajpandey22 <pankajpandey22@proton.me>
2026-09-04 18:00:42 +0530

    Create Remove_Duplicates_from_Sorted_Array.java

---
+++ Remove_Duplicates_from_Sorted_Array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int l = 0;
+        int r = 1;
+        while (r<nums.length) {
+            if (nums[r]!=nums[r-1]) {
+                nums[l+1] = nums[r];
+                l++;
+            }
+            r++;
+        }
+        return l+1;
+    }
+}
